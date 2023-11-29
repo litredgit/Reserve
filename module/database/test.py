e1d1ef5993d4ba6c11e41705e57bb4c1ceddb9ce
litredgit <2418848397@qq.com>
2023-11-29 15:06:26 +0800

    231129 json

--- module/database/test.py
+++ module/database/test.py
@@ -6,9 +6,10 @@ def testprint():
 _path = os.path.abspath(__file__)
 for i in range(3):
     _path = os.path.dirname(_path)
-settings_path = os.path.join(_path, 'database', 'settings.json')
+db_path = {'db_path': os.path.join(_path, 'database', 'reserve.db').replace('\\', '/')}
 
-with open(settings_path, 'r') as f:
-    db_path = json.load(f)['db_path']
-    
 print(db_path)
+with open("settings.json", "w") as f:
+    #db_path = json.load(f)['db_path']
+    json.dump(db_path, f)
+    # print(json.load(f)['db_path'])
