42158eb300f8ffcdb9fb45925db85f2a62f4d4aa
litredgit <2418848397@qq.com>
2023-11-29 14:29:02 +0800

    231129, relative path

--- module/database/db.py
+++ module/database/db.py
@@ -1,4 +1,9 @@
-import sqlite3, json
+import sqlite3, json, os
+
+_path = os.path.abspath(__file__)
+for i in range(3):
+    _path = os.path.dirname(_path)
+settings_path = os.path.join(_path, 'database', 'settings.json')
 
 with open('D:/Works/Git demo/db/settings.json', 'r') as f:
     db_path = json.load(f)['db_path']
--- module/database/test.py
+++ module/database/test.py
@@ -1,2 +1,14 @@
+import os, json
+
 def testprint():
     print("test done")
+    
+_path = os.path.abspath(__file__)
+for i in range(3):
+    _path = os.path.dirname(_path)
+settings_path = os.path.join(_path, 'database', 'settings.json')
+
+with open(settings_path, 'r') as f:
+    db_path = json.load(f)['db_path']
+    
+print(db_path)
